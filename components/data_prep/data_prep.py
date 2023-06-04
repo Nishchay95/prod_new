113f11faa82102e4ddfb329f70ab794d91b8f4b0
Praneeth <64009514+praneeth300@users.noreply.github.com>
2023-06-04 11:43:53 +0530

    Update data_prep.py

--- components/data_prep/data_prep.py
+++ components/data_prep/data_prep.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import logging
 import mlflow
-
+print("data preprocessing completed")
 
 def main():
     """Main function of the script."""
